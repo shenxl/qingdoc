80d4a44f66708efab062452b52e56da35548124a
shenxl <shen.sxl@icloud.com>
2014-12-12 16:30:22 +0800

    去除了Microsoft.Practices.Unity.Configuration的引用

--- shenxl.qingdoc/App_Start/Bootstrapper.cs
+++ shenxl.qingdoc/App_Start/Bootstrapper.cs
@@ -7,7 +7,6 @@ using System.Web.Mvc;
 using Microsoft.Practices.Unity;
 using shenxl.qingdoc.IoC;
 using shenxl.qingdoc.Common.DataAccess;
-using Microsoft.Practices.Unity.Configuration;
 using System.Configuration;
 
 namespace shenxl.qingdoc
